aa568df32fe6e828be181eceb4f05d8ff90c738a
Johann Alban Schoepfer <johann.schoepfer@tum.de>
2020-08-25 17:06:29 +0200

    Integrated PouchDB, added HumbleObj "AppDisplay" to mock database

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import PouchDB from 'pouchdb'
 import './App.css';
-import './elements/ActionList'
 import ActionList from './elements/ActionList';
 
-function App() {
-  let TODOs = ['Take out trash', 'read GTD chapter']
+
+function addTodo(db, text, todoIDCtr) {
+  var todo = {
+    _id: '' + todoIDCtr,
+    title: text,
+    completed: false
+  };
+  db.put(todo, function callback(err, result) {
+    if (!err) {
+      console.log('Successfully posted a todo: ' + text);
+    } else {
+      console.log('Error while posting todo: ' + err)
+    }
+  });
+  return todoIDCtr + 1
+}
+
+function showTodos(db, callback) {
+  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+    if (!err) {
+      var todos = doc.rows.map(row => row.doc.title)
+      callback(todos)
+    } else {
+      console.log('Error while loading files: ' + err)
+    }
+  });
+}
+
+export function AppDisplay(props) {
+
+  const [todos, updateTodos] = useState([])
+  useEffect(function(){
+    props.db.showTodos(updateTodos)
+  }, [props.db])
 
   return (
     <div className="App">
@@ -13,10 +44,21 @@ function App() {
         Sweep your mind!
       </header>
       <main>
-        <ActionList actions={TODOs}/>
+        <ActionList actions={todos}/>
       </main>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  var db = new PouchDB('todos');
+  var todoIDCtr = 0
+  todoIDCtr = addTodo(db, 'Take out trash', todoIDCtr)
+  todoIDCtr = addTodo(db, 'read DDD', todoIDCtr)
+
+  const dbObj = {
+    showTodos: function(cb){ showTodos(db, cb) }
+  }
+
+  return <AppDisplay db={dbObj}/>
+}
--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App, { AppDisplay } from './App';
+
+
+function mockDatabase(){
+  var todos = []
+  function showTodos(cb){cb(todos)}
+  function addTodo(text){todos.push(text)}
+  return {showTodos: showTodos, addTodo: addTodo}
+}
 
 test('renders header', () => {
-  const { getByText } = render(<App />);
+  const db = mockDatabase()
+  const { getByText } = render(<AppDisplay db={db}/>);
   const linkElement = getByText(/sweep your mind/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders TODO items', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/take out trash/i);
+  const db = mockDatabase()
+  db.addTodo('take out trash')
+  const { getByText } = render(<AppDisplay db={db}/>);
+  const linkElement = getByText(/take out trash/);
   expect(linkElement).toBeInTheDocument();
 });
