1c9e7620878b9da9b959be311174974cf833708b
Johann Alban Schöpfer <johann.schoepfer@tum.de>
2020-10-25 12:37:10 +0100

    again!

--- src/App.js
+++ src/App.js
@@ -1,88 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import PouchDB from 'pouchdb'
-import ActionList from './elements/ActionList';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-function addTodo(db, text, todoID) {
-  var todo = {
-    _id: '' + todoID,
-    title: text,
-    completed: false
-  };
-  db.put(todo, function callback(err, result) {
-    if (err) {
-      console.log('Error while posting todo (' + text + ', ' + todoID + '): ' + err)
-    }
-  });
-}
-
-function showTodos(db, callback) {
-  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-    if (!err) {
-      var todos = doc.rows.map(row => row.doc.title)
-      callback(todos)
-    } else {
-      console.log('Error while loading todos: ' + err)
-    }
-  });
-}
-
-function* idMaker() {
-  var idCtr = 0
-  while (true) {
-    yield idCtr
-    idCtr += 1
-  }
-}
-
-export function AppDisplay(props) {
-
-  const [todos, updateTodos] = useState([])
-  useEffect(function(){
-    props.db.showTodos(updateTodos)
-  }, [props.db])
-
+function App() {
   return (
-    <div style={appStyle}>
-      <header style={appHeaderStyle}>
-        Sweep your mind!
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <main style={appBodyStyle}>
-        <ActionList actions={todos}/>
-      </main>
     </div>
   );
 }
 
-const appStyle = {
-  'display': 'flex',
-  'flex-direction': 'column',
-  'height': '100%'
-}
-
-const appHeaderStyle = {
-  'background-color': '#282c34',
-  'min-height': '10vh',
-  'text-align': 'center',
-  'font-size': 'calc(10px + 2vmin)',
-  'color': 'white'
-}
-
-const appBodyStyle = {
-  'flex-grow': '1'
-}
-
-function App() {
-  var db = new PouchDB('todos');
-  var ids = idMaker();
-  addTodo(db, 'Take out trash', ids.next().value);
-  addTodo(db, 'read DDD', ids.next().value);
-
-  const dbObj = {
-    showTodos: function(cb){ showTodos(db, cb) }
-  }
-
-  return <AppDisplay db={dbObj}/>
-}
-
-export default App
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App, { AppDisplay } from './App';
+import App from './App';
 
-
-function mockDatabase(){
-  var todos = []
-  function showTodos(cb){cb(todos)}
-  function addTodo(text){todos.push(text)}
-  return {showTodos: showTodos, addTodo: addTodo}
-}
-
-test('renders header', () => {
-  const db = mockDatabase()
-  const { getByText } = render(<AppDisplay db={db}/>);
-  const linkElement = getByText(/sweep your mind/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders TODO items', () => {
-  const db = mockDatabase()
-  db.addTodo('take out trash')
-  const { getByText } = render(<AppDisplay db={db}/>);
-  const linkElement = getByText(/take out trash/);
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
