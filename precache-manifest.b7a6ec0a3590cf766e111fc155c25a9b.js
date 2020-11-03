03e284d2153c5d28711d7bd72417e92da778648d
jaschoepfer <johann.schoepfer@tum.de>
2020-11-03 19:16:34 +0000

    Updates

---
+++ precache-manifest.b7a6ec0a3590cf766e111fc155c25a9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758f24ff5f71a2319879b48eda7ab3aa",
+    "url": "/mind-broom/index.html"
+  },
+  {
+    "revision": "a8a2a0190ab9b999d038",
+    "url": "/mind-broom/static/css/main.624c5b16.chunk.css"
+  },
+  {
+    "revision": "da45ba7fa88c046fe50d",
+    "url": "/mind-broom/static/js/2.75d44d9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mind-broom/static/js/2.75d44d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a2a0190ab9b999d038",
+    "url": "/mind-broom/static/js/main.49398bad.chunk.js"
+  },
+  {
+    "revision": "6733eb4829fdb2c28f63",
+    "url": "/mind-broom/static/js/runtime-main.cf7e8b9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mind-broom/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48e4b7af4ff7af2cbda8f14bd73ec4d5.js"
+  "/mind-broom/precache-manifest.b7a6ec0a3590cf766e111fc155c25a9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mind-broom/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
