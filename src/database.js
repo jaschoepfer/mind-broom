1dbd9161506da2aa75ce1b227c44633feee453af
Johann Alban Schöpfer <johann.schoepfer@tum.de>
2020-11-16 20:28:24 +0100

    added database.js with create/destroyItem

--- src/database.js
+++ src/database.js
@@ -1,4 +1,5 @@
 import PouchDB from 'pouchdb'
+import { v4 as uuid } from 'uuid'
 
 var lists
 var items
@@ -10,7 +11,7 @@ export async function initialize(testOptions={}) {
         ...pouchOptions,
         ...testOptions
     }
-    console.log(options)
+    console.log('using PouchDB with options:', options)
     lists = new PouchDB('lists', options)
     items = new PouchDB('items', options)
     // perform an actual request to verify the DB talks to us
@@ -18,4 +19,24 @@ export async function initialize(testOptions={}) {
     let itemsInfo = items.info()
     await listsInfo
     await itemsInfo
+}
+
+const ID_SEP = '/'
+const combineID = (parentID, elemID) => parentID + ID_SEP + elemID
+const splitID = (combinedID) => combinedID.split(ID_SEP)
+
+const genItemID = (parentID) => combineID(parentID, uuid())
+
+export async function createItem(listID, item) {
+    let doc = {
+        ...item,
+        _id: genItemID(listID)
+    }
+    let { rev } = await items.put(doc)
+    doc._rev = rev
+    return doc
+}
+
+export async function deleteItem(itemID, revision) {
+    await items.remove(itemID, revision)
 }
--- src/database.test.js
+++ src/database.test.js
@@ -1,5 +1,6 @@
 import * as db from './database.js'
 import PouchDB from 'pouchdb'
+import { validate } from 'uuid'
 
 beforeAll(async () => {
     // use an in-memory pouchdb implementation
@@ -7,4 +8,14 @@ beforeAll(async () => {
     await db.initialize({adapter: 'memory'})
 })
 
-test('implement later', () => {})
+test('create item in default list', async () => {
+    let parent = 'default'
+    let payload = { text: 'breathe motherf*#!r' }
+    let doc = await db.createItem(parent, payload)
+    let [ parentID, UUID ] = doc._id.split('/')
+    expect(parentID).toBe(parent)
+    expect(validate(UUID)).toBe(true)
+    delete doc._id
+    delete doc._rev
+    expect(doc).toEqual(payload)
+})
